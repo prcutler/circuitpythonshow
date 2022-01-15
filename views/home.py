99e549f6b485b34f3be7f783e64d5db6dcaf7dcb
Paul Cutler <paul@paulcutler.org>
2022-01-15 13:24:07 -0600

    Add subscirption thank you

--- views/home.py
+++ views/home.py
@@ -21,3 +21,10 @@ def index(request: Request):
 def index(request: Request):
     vm = IndexViewModel(request)
     return vm.to_dict()
+
+
+@router.get("/thank-you-for-subscribing")
+@template(template_file="home/thank-you-for-subscribing.pt")
+def index(request: Request):
+    vm = IndexViewModel(request)
+    return vm.to_dict()
