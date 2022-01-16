ba766ca2af6f39a84cb23f25fbfd4f67c517b92d
Paul Cutler <paul@paulcutler.org>
2022-01-16 09:59:51 -0600

    First draft of episode class

--- data/episode.py
+++ data/episode.py
@@ -1,37 +1,24 @@
-from typing import List
+from datetime import datetime
+from typing import List, Optional
 
 
 class AlbumInfo:
     def __init__(
         self,
-        release_id,
-        release_url: str,
-        artist_id: int,
-        artist_name: str,
-        release_title: str,
-        artist_url: str,
-        release_image_url: str,
-        genres: List[str],
-        # discogs_main_id: str,
-        # discogs_main_url: str,
-        main_release_date: int,
-        album_release_date: str,
-        track_title: str,
-        track_duration: str,
-        track_position: str,
-    ):
-        self.release_id = release_id
-        self.release_url = release_url
-        self.artist_id = artist_id
-        self.artist_name = artist_name
-        self.release_title = release_title
-        self.artist_url = artist_url
-        self.release_image_url = release_image_url
-        self.genres = genres
-        # self.discogs_main_id: int = discogs_main_id
-        # self.discogs_main_url = discogs_main_url
-        self.main_release_date: int = main_release_date
-        self.album_release_date = album_release_date
-        self.track_title = track_title
-        self.track_duration = track_duration
-        self.track_position = track_position
+        record_date: datetime.date,
+        release_date: datetime.date,
+        guest: str,
+        topic: str,
+        season: int,
+        episode: int,
+        guest_image: Optional[str],
+        guest_bio: Optional[str],
+   ):
+        self.record_date = release_date
+        self.release_date = release_date
+        self.guset = guest
+        self.topic = topic
+        self.season = season
+        self.episode = episode
+        self.guest_image = guest_image
+        self.guest_bio = guest_bio
