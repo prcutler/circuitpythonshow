e493e71786f5850ae2896f14546b29d07ce8a3d3
Paul Cutler <paul@paulcutler.org>
2022-01-23 05:28:19 -0600

    Continue to work on database creation

--- data/episode.py
+++ data/episode.py
@@ -1,24 +1,22 @@
-from datetime import datetime
+import datetime
 from typing import List, Optional
+import sqlalchemy as sa
 
+from data.modelbase import SqlAlchemyBase
 
-class Episode:
-    def __init__(
-        self,
-        record_date: datetime.date,
-        release_date: datetime.date,
-        guest: str,
-        topic: str,
-        season: int,
-        episode: int,
-        guest_image: Optional[str],
-        guest_bio: Optional[str],
-   ):
-        self.record_date = record_date
-        self.release_date = release_date
-        self.guest = guest
-        self.topic = topic
-        self.season = season
-        self.episode = episode
-        self.guest_image = guest_image
-        self.guest_bio = guest_bio
+class Episode(SqlAlchemyBase):
+    
+    __tablename__='episodes'
+    
+    id: str = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    guest_firstname: str = sa.Column(sa.String)
+    guest_lastname: str = sa.Column(sa.String)
+    record_date: datetime.datetime = sa.Column(sa.DateTime)
+    publish_date: datetime.datetime = sa.Column(sa.DateTime)
+    topic: str = sa.Column(sa.String)
+    season: int = sa.Column(sa.Integer)
+    episode: int = sa.Column(sa.Integer)
+    guest_image: str = sa.Column(sa.String)
+    guest_bio: str = sa.Column(sa.String)
+    
+    
--- data/user.py
+++ data/user.py
@@ -1,9 +1,16 @@
 import datetime
+import sqlalchemy as sa
+
+from data.modelbase import SqlAlchemyBase
 
 
-class User:
-    def __init__(self, username, password):
-        # self.id = 1
-        self.username = username
-        self.password = password
-        # self.last_login: datetime.datetime = None
+class User(SqlAlchemyBase):
+    
+    __tablename__ = 'users'
+    
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String)
+    email = sa.Column(sa.String, index=True, unique=True)
+    created_date = sa.Column(sa.DateTime)
+    
+
