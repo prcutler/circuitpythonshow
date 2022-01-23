3aff31dbb47e6ed68f005b8447af2b808eba57bd
Paul Cutler <paul@paulcutler.org>
2022-01-23 05:53:44 -0600

    Update user class with types

--- data/user.py
+++ data/user.py
@@ -8,9 +8,10 @@ class User(SqlAlchemyBase):
     
     __tablename__ = 'users'
     
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    name = sa.Column(sa.String)
-    email = sa.Column(sa.String, index=True, unique=True)
-    created_date = sa.Column(sa.DateTime)
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    username: str = sa.Column(sa.String)
+    email: str = sa.Column(sa.String, index=True, unique=True)
+    created_date: datetime.datetime = sa.Column(sa.DateTime)
+    su: bool = sa.Column(sa.Boolean)
     
 
