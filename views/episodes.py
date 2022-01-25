b319364ad1f65fd47341fe89828eab80bd47c06f
Paul Cutler <paul@paulcutler.org>
2022-01-25 07:08:09 -0600

    Add Trailer

--- views/episodes.py
+++ views/episodes.py
@@ -13,3 +13,13 @@ def all():
 @template(template_file="episodes/episode-template.pt")
 def all():
     return {}
+
+@router.get("/episodes/episodes/0/trailer")
+@template(template_file="episodes/episode0-trailer.pt")
+def all():
+    return {}
+
+@router.get("/episodes/0/transcripts/episode0-transcript")
+@template(template_file="episodes/transcripts/episode0-transcript.pt")
+def all():
+    return {}
