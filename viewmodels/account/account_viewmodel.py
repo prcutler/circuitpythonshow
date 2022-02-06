08c0b044f1be2f219aaded02ca34d262b7ec1c2a
Paul Cutler <paul@paulcutler.org>
2022-02-06 09:59:35 -0600

    Login works; user query bad

--- viewmodels/account/account_viewmodel.py
+++ viewmodels/account/account_viewmodel.py
@@ -11,8 +11,7 @@ class AccountViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
         self.user: Optional[User] = None
-        self.email = None
+        
 
     async def load(self):
         self.user = await user_service.get_user_by_id(self.user_id)
-
