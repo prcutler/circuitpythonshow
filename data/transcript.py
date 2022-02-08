5401664c654c91d71942078fd6f24fc9a606b443
Paul Cutler <paul@paulcutler.org>
2022-02-07 21:25:55 -0600

    Comment out picture and add transcript table

---
+++ data/transcript.py
@@ -0,0 +1,15 @@
+import datetime
+from typing import List, Optional
+import sqlalchemy as sa
+
+from data.modelbase import SqlAlchemyBase
+
+
+class Episode(SqlAlchemyBase):
+
+    __tablename__ = "transcripts"
+    
+    season: int = sa.Column(sa.Integer)
+    episode_number: int = sa.Column(sa.Integer, index=True)
+    transcript_1: str = sa.Column(sa.String)
+    transcript_2: str = sa.Column(sa.String)
