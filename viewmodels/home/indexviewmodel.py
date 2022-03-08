081a40f302a31d3a1f6143f257f47775d3c170c6
Paul Cutler <paul@paulcutler.org>
2022-03-08 04:21:52 -0600

    Merge pull request #29
    * Add 2nd guest to front page - use lists!
    
    * Format dates correctly for front page
    
    * Merge branch 'dev' into 2news
    
    * Merge pull request #25 from prcutler/2news
    
    * Update home page graphics

--- viewmodels/home/indexviewmodel.py
+++ viewmodels/home/indexviewmodel.py
@@ -19,6 +19,10 @@ class IndexViewModel(ViewModelBase):
         self.guest_lastname: str = ""
 
         self.topic: str = ""
+        
+        self.old_episode_number: Optional[int] = None
+        self.old_publish_date: Optional[str] = None
+        
         self.publish_date: str = ""
 
         self.old_episode_number: Optional[int] = None
@@ -30,6 +34,15 @@ class IndexViewModel(ViewModelBase):
 
         self.episode_count: int = await episode_service.get_episode_count()
         self.episode_number = await episode_service.get_last_episode_number()
+        
+        self.episodes = await episode_service.get_episode_info(self.episode_number)
+        self.publish_date = await episode_service.get_publish_date(self.episode_number)
+        
+        self.old_episode_number = self.episode_number - 1
+        print("Old ep #", self.old_episode_number)
+        
+        self.old_episode = await episode_service.get_episode_info(self.old_episode_number)
+        self.old_publish_date = await episode_service.get_publish_date(self.old_episode_number)
 
         self.episodes = await episode_service.get_episode_info(self.episode_number)
         self.publish_date = await episode_service.get_publish_date(self.episode_number)
@@ -45,3 +58,4 @@ class IndexViewModel(ViewModelBase):
         self.old_publish_date = await episode_service.get_publish_date(
             self.old_episode_number
         )
+
